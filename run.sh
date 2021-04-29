d5cc122e8f59f55ba1447436119c2231fe79d3c3
神雕 <teasiu@qq.com>
2021-04-30 01:06:38 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./ngrokd -httpAddr="0.0.0.0:80" -httpsAddr="0.0.0.0:443" -tunnelAddr=":4443" -domain="$SUBDOMAIN" -authToken="$TOKEN"
+./ngrokd -domain="herokuapp.com" -httpAddr=":8081" -httpsAddr=":8082" -authToken="$TOKEN"
