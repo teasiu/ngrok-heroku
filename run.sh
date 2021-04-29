3a2ebd1bfd85a4e8056a8214618ff714e2618b37
神雕 <teasiu@qq.com>
2021-04-30 00:24:40 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./ngrokd -httpAddr="0.0.0.0:8000" -httpsAddr="0.0.0.0:1443" -tunnelAddr=":4443" -domain="$SUBDOMAIN" -authToken="$TOKEN"
+./ngrokd -httpAddr="0.0.0.0:80" -httpsAddr="0.0.0.0:443" -tunnelAddr=":4443" -domain="$SUBDOMAIN" -authToken="$TOKEN"
